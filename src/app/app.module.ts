f95272f378a9f2caaab80f5a680ec0a03d81c820
liu yi <lilugirl2005@126.com>
2021-03-29 22:25:55 +0800

    封装组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScrollableTabComponent } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScrollableTabComponent
   ],
   imports: [
     BrowserModule,
