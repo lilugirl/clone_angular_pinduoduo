9b6ca5ac94c458b2fbd3ee51cf8c283560faa218
liu yi <lilugirl2005@126.com>
2021-03-29 23:05:12 +0800

    封装头部菜单组件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TopMenu} from './components'
 
 
 @Component({
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+ topMenus:TopMenu[]=[
+  {title:'男装',url:'#1'},
+  {title:"儿童",url:'#2'},
+  {title:'百货',url:''},
+  {title:'手机',url:''},
+  {title:'日用',url:''},
+  {title:'美妆',url:''},
+  {title:'家电',url:''},
+  {title:'运动',url:''},
+  {title:'百货',url:''},
+  {title:'手机',url:''},
+  {title:'日用',url:''},
+  {title:'美妆',url:''},
+  {title:'家电',url:''},
+  {title:'运动',url:''},
+  {title:'家电',url:''},
+  {title:'运动',url:''},
+  {title:'百货',url:''},
+  {title:'手机',url:''},
+  {title:'日用',url:''},
+  {title:'美妆',url:''},
+  {title:'家电',url:''},
+  {title:'运动',url:''},
+ ]
+
+ handleTabSelected(topMenu:TopMenu){
+   console.log('handleTabSelected',topMenu);
+ }
 }
--- src/app/components/scrollable-tab/scrollable-tab.component.ts
+++ src/app/components/scrollable-tab/scrollable-tab.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-interface TopMenu{
+
+export interface TopMenu{
   title:string;
    url?:string;
  }
  
- 
- 
- interface Dict{
-   [key:number]:string;
- }
- 
+
 @Component({
   selector: 'app-scrollable-tab',
   templateUrl: './scrollable-tab.component.html',
@@ -20,47 +16,14 @@ export class ScrollableTabComponent implements OnInit {
 
   selectedIndex=-1;
   title = 'pinduoduo';
-  tabs:TopMenu[]=[
-  {title:'男装',url:'#1'},
-  {title:"儿童",url:'#2'},
-  {title:'百货',url:''},
-  {title:'手机',url:''},
-  {title:'日用',url:''},
-  {title:'美妆',url:''},
-  {title:'家电',url:''},
-  {title:'运动',url:''},
-  {title:'百货',url:''},
-  {title:'手机',url:''},
-  {title:'日用',url:''},
-  {title:'美妆',url:''},
-  {title:'家电',url:''},
-  {title:'运动',url:''},
-  {title:'家电',url:''},
-  {title:'运动',url:''},
-  {title:'百货',url:''},
-  {title:'手机',url:''},
-  {title:'日用',url:''},
-  {title:'美妆',url:''},
-  {title:'家电',url:''},
-  {title:'运动',url:''},
-];
+  @Input() tabs:TopMenu[]=[];
+  @Output() tabSelected =new EventEmitter();
 
-  dict:Dict={
-    1:'aaaa',
-    2:'bbbb'
-
-  }
-
-
-
- constructor(){
-  console.log(this.dict[1]);
-  console.log(this.dict[2]);
- }
-
- handleSelection(index:any){
-   console.log(index);
+ 
+ handleSelection(index:number){
    this.selectedIndex=index;
+   this.tabSelected.emit(this.tabs[this.selectedIndex]);
+
  }
 
   ngOnInit(): void {
