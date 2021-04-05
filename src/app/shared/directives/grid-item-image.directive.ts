13d4cc1d81bc5f6bfeb95bbd88a6aacc15a46fa7
liu yi <lilugirl2005@126.com>
2021-04-05 16:49:20 +0800

    重构grid-item指令

--- src/app/shared/directives/grid-item-image.directive.ts
+++ src/app/shared/directives/grid-item-image.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
+import { Directive, ElementRef,  HostListener,  Input, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[appGridItemImage]'
@@ -7,13 +7,19 @@ import { Directive, ElementRef, Input, OnInit, Renderer2 } from "@angular/core";
 export class GridItemImageDirective implements OnInit{
     @Input() appGridItemImage='2rem';
     @Input() fitMode='cover';
+   
     constructor(private elr:ElementRef,private rd2:Renderer2){
-          }
+    }
     ngOnInit():void{
         this.rd2.setStyle(this.elr.nativeElement,'grid-area','image');
         this.rd2.setStyle(this.elr.nativeElement,'width',this.appGridItemImage);
         this.rd2.setStyle(this.elr.nativeElement,'height',this.appGridItemImage);
         this.rd2.setStyle(this.elr.nativeElement,'object-fit',this.fitMode);
  
+    }
+
+    @HostListener('click',['$event.target'])
+    handleClick(ev:any){
+      console.log('ev',ev);
     }
 }
--- src/app/shared/directives/grid-item-title.directive.ts
+++ src/app/shared/directives/grid-item-title.directive.ts
@@ -1,13 +1,9 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
+import { Directive,  HostBinding, Input } from "@angular/core";
 
 @Directive({
     selector:'[appGridItemTitle]'
 })
-export class GridItemTitleDirective implements OnInit{
-    constructor(private elr:ElementRef,private rd2:Renderer2){
-        
-    }
-    ngOnInit():void{
-        this.rd2.setStyle(this.elr.nativeElement,'grid-area','title');
-    }
+export class GridItemTitleDirective {
+    @HostBinding('style.grid-area')  area='title';
+    @HostBinding('style.font-size') @Input() appGridItemTitle='0.5rem';  
 }
